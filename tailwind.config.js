93172ebea38cd3990fd37d1bbfeec1206668a0d8
Aminadi2296 <moroniaminadi@gmail.com>
2024-07-17 16:10:19 -0400

    adding files to repository

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        "resp-8": "8vw",
+        "resp-24": "24vw"
+      }
+    },
+  },
+  plugins: [],
+}
+
